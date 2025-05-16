ea6f0ae4b7b140da9cb5d274821260d7c0d2aec1
Roaa <emamroka80@gmail.com>
2025-05-16 15:57:38 -0700

    task3

--- module1/build.gradle.kts
+++ module1/build.gradle.kts
@@ -1,6 +1,20 @@
-group = rootProject.group
-version = rootProject.version
+plugins {
+    kotlin("jvm") version "1.9.10"
+}
+
+repositories {
+    mavenCentral()
+}
 
 dependencies {
-    project(":module2")
+    implementation(project(":module2"))
+
+    // JUnit 5 dependencies
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
--- module2/build.gradle.kts
+++ module2/build.gradle.kts
@@ -1,2 +1,16 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+}
+
 group = rootProject.group
 version = rootProject.version
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+}
